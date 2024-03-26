6b03074adad31647faec2353fe33fd687302cc04
msd984 <165062158+msd984@users.noreply.github.com>
2024-03-26 22:17:00 +0530

    Add files via upload

---
+++ daa/queens.c
@@ -0,0 +1,59 @@
+#include<conio.h>
+ #include<math.h>
+ int x[30],count=0;
+ int place(int pos)
+ {
+ int i;
+ for (i=1;i<pos;i++) {
+ if((x[i]==x[pos]) |((abs(x[i]-x[pos])==abs(i-pos))))
+ return 0;
+ }
+ return 1;
+ }
+ void printboard(int n)
+ {
+ int i,j;
+ count++;
+ printf("\n\nsolution%d:\n",count);
+ for (i=1;i<=n;i++)
+ {
+ for (j=1;j<=n;j++)
+ {
+ if(x[i]==j)
+ printf("Q%d\t",i); else
+ printf("*\t");
+ }
+ printf("\n");
+ }
+ }
+ void queen(int n)
+ {
+ int k=1;
+ x[k]=0;
+ while(k!=0)
+ {
+ x[k]=x[k]+1;
+ while((x[k]<=n)&&!place(k))
+ x[k]++;
+ if(x[k]<=n)
+ {
+ if(k==n)
+ printboard(n);
+ else
+ {
+ }
+ } else
+ k--;
+ }
+ k++;
+ x[k]=0;
+ }
+void main()
+ {
+ int i,n;
+ printf("enter the number of queens..:");
+ scanf("%d",&n);
+ queen(n);
+ printf("\npossible solutions..: %d",count);
+ getch();
+ }
